bca36a821ab7aa0c0cdd741cb1be196c33b172af
Nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-05-28 14:41:46 +0200

    Add constant variables and constructor

--- src/main/java/io/github/lindelwa122/world/World.java
+++ src/main/java/io/github/lindelwa122/world/World.java
@@ -1,5 +1,23 @@
 package io.github.lindelwa122.world;
 
-public class World {
+import io.github.lindelwa122.coords.Coords;
 
+public class World {
+    private final int HEIGHT;
+    private final int WIDTH;
+
+    private final Coords DRY_CLIMATE_ORIGIN;
+    private final Coords COLD_CLIMATE_ORIGIN;
+    private final Coords WET_CLIMATE_ORIGIN;
+    private final Coords HOT_CLIMATE_ORIGIN;
+
+    public World(int height, int width) {
+        this.HEIGHT = height;
+        this.WIDTH = width;
+
+        this.DRY_CLIMATE_ORIGIN = new Coords(0, 0);
+        this.COLD_CLIMATE_ORIGIN = new Coords(width-1, 0);
+        this.WET_CLIMATE_ORIGIN = new Coords(0, height-1);
+        this.HOT_CLIMATE_ORIGIN = new Coords(height-1, width-1);
+    }
 }
