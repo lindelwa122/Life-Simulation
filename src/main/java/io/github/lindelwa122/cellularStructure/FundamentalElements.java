00faf031335ffbd1800384302fa71e503a2b16b1
Nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-05-30 09:30:57 +0200

    Add the Fundamental Elements

---
+++ src/main/java/io/github/lindelwa122/cellularStructure/FundamentalElements.java
@@ -0,0 +1,6 @@
+package io.github.lindelwa122.cellularStructure;
+
+public enum FundamentalElements {
+    HYDREX, IGNYRA, XERAPHIN, HUMIDRA, CRYONEL, LUNAPHYLL, XYLORA, 
+    MORBIORA, CARNYXIS, PREDONIX;
+}
