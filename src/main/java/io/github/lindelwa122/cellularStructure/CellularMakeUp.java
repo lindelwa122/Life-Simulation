33e1fefdfb6a66f8611eea5cf54216f649b349e4
Nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-06-20 09:09:06 +0200

    Add cellular makeup

---
+++ src/main/java/io/github/lindelwa122/cellularStructure/CellularMakeUp.java
@@ -0,0 +1,12 @@
+package io.github.lindelwa122.cellularStructure;
+
+public record CellularMakeUp(
+    Element hydrex, 
+    Element ignyra, 
+    Element xeraphin, 
+    Element humidra, 
+    Element cryonel, 
+    Element lunaphyll, 
+    Element xylora, 
+    Element morbiora, 
+    Element carnyxis) {}
